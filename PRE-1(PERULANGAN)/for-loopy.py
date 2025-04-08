451b7622f65dfa4778d92e2dfdec64b51b919518
Dini-ptrb <dynidini123@gmail.com>
2025-04-08 12:03:24 +0800

    Add files via upload

---
+++ PRE-1(PERULANGAN)/for-loopy.py
@@ -0,0 +1,30 @@
+# perulangan
+
+# for kondisi: JIKA KONDISI TERPENUHI 
+#     aksi JALANKAN AKSI 
+
+angka_list = [0,2,4,8,10] # ini adalah list
+print(angka_list)
+
+#angka = 0 
+#print (angka)
+#angka += 1 # angka = angka + 1
+#print (angka)
+#angka += 1
+#print (angka)
+
+#for_loop
+for i in angka_list : 
+    print(f"i sekarang -> (i)")
+
+print("loop berakhir \n")
+
+# dengan range 
+for 1 in range (5) :
+    print(f"i sekarang -> (1)")
+
+print("loop  range berakhir \n")
+for 2 in range (1,10):
+    print(f"i sekarang -> (2)")
+
+print("loop range berakhir \n")
